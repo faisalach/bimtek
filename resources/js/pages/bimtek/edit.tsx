663e7ae8c7977931312fc946d1172476a1151ae1
faisalach <faisalachramsyah@gmail.com>
2026-01-09 11:46:56 +0700

    fix routes not use

--- resources/js/pages/bimtek/edit.tsx
+++ resources/js/pages/bimtek/edit.tsx
@@ -1,5 +1,4 @@
 import BimtekController from '@/actions/App/Http/Controllers/BimtekController';
-import { send } from '@/routes/verification';
 import { type BreadcrumbItem, type SharedData } from '@/types';
 import { Transition } from '@headlessui/react';
 import { Form, Head, Link, usePage } from '@inertiajs/react';
